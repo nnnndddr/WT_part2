a1d4d99a57b801e1ff6aa55d8675d78c7797e3d6
nnnndddr <92089447+nnnndddr@users.noreply.github.com>
2022-11-17 21:38:30 +0300

    Add files via upload

---
+++ 2task/JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"by.tc.task01"},{"l":"by.tc.task01.dao"},{"l":"by.tc.task01.dao.impl"},{"l":"by.tc.task01.entity"},{"l":"by.tc.task01.enums"},{"l":"by.tc.task01.service"},{"l":"by.tc.task01.service.impl"}];updateSearchResults();
